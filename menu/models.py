999197cac598268e72f7af4b246bc1f25da71708
a-rod85 <31aaronanderson@gmail.com>
2023-02-06 13:01:27 +0000

    add template folder add base.html add menus_pages.html

--- menu/models.py
+++ menu/models.py
@@ -9,13 +9,16 @@ Drinks_Menu_Section = (
 )
 
 # Create your models here.
+
+
 class FoodItem(models.Model):
 
     dish_id = models.AutoField(primary_key=True)
-    dish_name = maodels.Charfield(max_length=220, unique=True)
+    dish_name = models.Charfield(max_length=220, unique=True)
     description = models.Charfield(max_length=220, unique=True)
     price = models.FloatField()
-    Food_Menu_Section = models.IntergerField(choices=Food_Menu_Section, default=1)
+    Food_Menu_Section = models.IntergerField(
+        choices=Food_Menu_Section, default=1)
     on_menu = models.BooleanField(default=False)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -25,14 +28,15 @@ class FoodItem(models.Model):
     def __str__(self):
         return self.dish_name
 
+
 class DrinkItem(models.Model):
-     
 
     drink_id = models.AutoField(primary_key=True)
     drink_name = maodels.Charfield(max_length=100, unique=True)
     description = models.Charfield(max_length=200, unique=True)
     price = models.FloatField()
-    Drink_menu_section = models.IntergerField(choices=Food_Menu_Section, default=1)
+    Drink_menu_section = models.IntergerField(
+        choices=Food_Menu_Section, default=1)
     on_menu = models.BooleanField(default=False)
     updated_on = models.DateTimeField(auto_now=True)
 
--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import FoodItem, DrinkItem
+
+
+def menus(request, 'menus_page.html')
+
+
+class FoodMenu(generic.ListView):
+
+    model = FoodItem
+    template_name = 'food_menu.html'
+    context_object_name = 'food_items'
+
+
+def get_queryset(self):
+    queryset = {
+        'dinner_items': FoodItem.objects.all().filter(
+            on_menu=True, food_menu_section=0
+        ),
+        'desserts': FoodItem.objects.all().filter(
+            on_menu=True, food_menu_section=1
+        ),
+    }
+    return queryset
+
+
+class DrinksMenu(generic.ListView):
+
+    model = DrinkItem
+    template_name = 'drinks_menu_html'
+    context_object_name = 'drinks_items'
+
+
+def get_queryset(self):
+    queryset = {
+        'Cocktails_items': DrinkItem.objects.all().filter(
+            on_menu=True, drink_menu_section=0
+        ),
+        'alcohol_items': DrinkItem.objects.all().filter(
+            on_menu=True, drink_menu_section=1
+        ),
+        'softdrinks_item': DrinkItem.objects.all().filter(
+            on_menu=True, drink_menu_section=2
+        ),
+    }
+    return queryset
