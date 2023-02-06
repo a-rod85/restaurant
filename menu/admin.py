cc751118f9116b3b8463148337693f4f6d8bdb22
a-rod85 <31aaronanderson@gmail.com>
2023-02-06 10:41:40 +0000

    startapp home startapp menu, added urls.py added views.py added to admin.py

---
+++ menu/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import FoodItem, DrinkItem
+
+
+# Register your models here.
+@admin.register(FoodItem)
+class FoodAdmin(admin.ModelAdmin):
+
+    list_filter = ('on_menu', 'updated_on')
+    list_display = ('dish_name', 'food_menu_section', 'price', 'on_menu')
+
+@admin.register(DrinkItem)
+class DrinkAdmin(admin.ModelAdmin):
+
+    list_filter = ('on_menu', 'updated_on')
+    list_display = ('drink_name', 'drinks_menu_section', 'price', 'on_menu')
